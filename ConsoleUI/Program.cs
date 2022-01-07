4c1f4364bf618c6498f89a2ef1a15c2320f7eade
Aaron <git@github.com:aromero082/PizzaAlgo.git>
2022-01-06 16:59:54 -0800

    AbstractVehicleClass

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -32,17 +32,35 @@ namespace ConsoleUI
             */
 
             // Create a list of Vehicle called vehicles
+            var vehicles = new List<Vehicle>();
 
             /*
              * Create 4 instances: 1 Car, 1 Motorcycle, and then 2 instances of type Vehicle (use explicit typing) but use constuctors from derived classes
              * - new it up as one of each derived class
              * Set the properties with object initializer syntax
              */
+            var civic = new Car() { HasTrunk = true, Make = "Honda", Model = "Civic", Year = 2017 };
+            var motorcycle = new Motorcycle() { HasSideCart = true, Make = "Indian", Model = "Scout", Year = 2020 };
+
+            Vehicle suv = new Car() { Make = "Nissan", Model = "Rougue", Year = 2016 };
+            Vehicle sporstcar = new Car() { HasTrunk = true, Make = "Bmw", Model = "520i", Year = 2018 };
+
 
             /*
              * Add the 4 vehicles to the list
              * Using a foreach loop iterate over each of the properties
              */
+            vehicles.Add(civic);
+            vehicles.Add(motorcycle);
+            vehicles.Add(suv);
+            vehicles.Add(sporstcar);
+
+            foreach (var veh in vehicles)
+            {
+                Console.WriteLine($" Make {veh.Make} Model {veh.Model} Year {veh.Year}");
+                veh.DriveVirtual();
+                Console.WriteLine("--------------------------------");
+            }
 
             // Call each of the drive methods for one car and one motorcycle
 
